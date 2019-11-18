19557ddf892786d9a91e2adefb7cc8856c463eca
NicoleBarra <38593156+NicoleBarra@users.noreply.github.com>
2019-11-18 15:29:57 -0600

    Reads txt file correctly

---
+++ NonTerminal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class NonTerminal{
+    List derivations;
+    char name;
+    public NonTerminal(char name){
+        this.derivations = new ArrayList();
+        this.name = name;
+    }
+
+    public List getDerivations(){
+        return this.derivations;
+    }
+
+    public void setDerivations(List derivations){
+        this.derivations = derivations;
+    }
+
+    public void addDerivation(String derivation){
+        derivations.add(derivation);
+    }
+
+    public char getName(){
+        return name;
+    }
+
+
+}
+
--- PushDownAutomaton.java
+++ PushDownAutomaton.java
@@ -26,42 +26,64 @@ public class PushDownAutomaton
 
 
     public static void main(String[] args) {
-    ArrayList<Character> nonTerminal = new ArrayList<Character>();
-    ArrayList<Character> terminal = new ArrayList<Character>();
+        Map<Character,NonTerminal> nonTerminalMap = new HashMap<>();
+        ArrayList<NonTerminal> nonTerminalList = new ArrayList<NonTerminal>();
+        ArrayList<Character> terminal = new ArrayList<Character>();
 
-    Scanner in = new Scanner(System.in);
-    System.out.println("Name of file(must be in the same folder): ");
-    String txt = in.nextLine(); 
-    //try catch
-    List<String> l = readFileInList(txt); 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Name of file(must be in the same folder): ");
+        String txt = in.nextLine(); 
+        //try catch
+        List<String> l = readFileInList(txt); 
 
-    int i = 0;
-    for(String s: l){
+        int i = 0;
+        for(String s: l){
 
-        i = i+1;
-        System.out.println(i);
-        System.out.println(s);
-        String[] data = s.split(",");
+            i = i+1;
+            System.out.println(i);
+            System.out.println(s);
+            
 
-      
+        
 
-        if( i == 1){
-            for(int j = 0; j<data.length;j++){
-                char a = data[j].charAt(0);
-                nonTerminal.add(a);
-            }
-        }
-
-        else if( i == 2){
-            for(int j = 0; j<data.length;j++){
-                char a = data[j].charAt(0);
-                terminal.add(a);
+            if( i == 1){
+                String[] data = s.split(",");
+                for(int j = 0; j<data.length;j++){
+                    char a = data[j].charAt(0);
+                    NonTerminal newTerminal = new NonTerminal(a);
+                    nonTerminalMap.put(a,newTerminal);
+                }
             }
 
-        }
-    
+            else if( i == 2){
+                String[] data = s.split(",");
+                for(int j = 0; j<data.length;j++){
+                    char a = data[j].charAt(0);
+                    terminal.add(a);
+                }
 
-      }
+            }
+
+            else if(i == 3){
+                char a = s.charAt(0);
+                char initial = a;
+            }
+
+            else{
+                String[] data = s.split("->");
+                nonTerminalMap.get(data[0].charAt(0)).addDerivation(data[1]);            
+            }
+
+            
+
+        }
+        for (Map.Entry<Character, NonTerminal> entry : nonTerminalMap.entrySet()) {
+                
+            System.out.println("Non-Terminal: " + entry.getValue().getName());
+            for(int k = 0; k< entry.getValue().getDerivations().size();k++){
+                System.out.println("Derivation " + k + ": " + entry.getValue().getDerivations().get(k));
+            }
+        }
 
     }
 
