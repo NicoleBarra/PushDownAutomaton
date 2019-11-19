7b64d09a78d2626a709d0cb761fbf12835941e28
NicoleBarra <38593156+NicoleBarra@users.noreply.github.com>
2019-11-18 18:56:25 -0600

    finally finished

--- PushDownAutomaton.java
+++ PushDownAutomaton.java
@@ -24,11 +24,88 @@ public class PushDownAutomaton
     return lines; 
     }
 
+    public static void makeTree(Map<Character,NonTerminal> nonTerminalMap, String initial, String p){
+        Queue<String> q = new LinkedList<>(); 
+        boolean done = false;
+        char firstNonTerminal = 'A';
+
+        q.add(initial);
+
+        while(q.size()>0 && done == false){
+            System.out.println("hewoo owo");
+
+            //get the first non-terminal symbol
+            String symbol = q.remove();
+            
+            for(int n = 0; n< symbol.length();n++){
+                if(nonTerminalMap.containsKey(symbol.charAt(n))){
+                    System.out.println("found the first Non Terminal");
+                    firstNonTerminal = symbol.charAt(n);
+
+                    break;
+                }
+            }
+
+            List<String> r = nonTerminalMap.get(firstNonTerminal).getDerivations();
+            for(int i = 0; i < r.size();i++){
+                System.out.println("i: " + i);
+                if(done == true){
+                    break;
+                }
+                String rule = r.get(i);
+                if(rule.equals("lmd")){
+                    rule = symbol.replaceFirst(String.valueOf(firstNonTerminal),"");
+                }
+                else{
+                    rule = symbol.replaceFirst(String.valueOf(firstNonTerminal),rule);
+                }
+                
+                
+                System.out.println("original symbol: " + symbol);
+                System.out.println("rule: " + rule);
+                //Check if the prefix is still the same as the p line, and if there are non-terminal characters
+                for(int j = 0; j<rule.length();j++){
+                    System.out.println("j: " + j);
+                    if(nonTerminalMap.containsKey(rule.charAt(j))){
+                        q.add(rule);
+                        break;
+
+                    }
+                    else{
+                        if(rule.charAt(j) == (p.charAt(j)) && rule.length()<=p.length()+1){
+                            continue;
+                        }
+                        else{
+                            
+                            break;
+                        }
+                        
+                    }
+                    
+                }
+                if(rule.equals(p)){
+                    done = true;
+                    System.out.println("done was found");
+                }
+
+
+
+            }
+        }
+
+        if(done == true){
+            System.out.println("Accepted");
+        
+        }
+        else{
+            System.out.println("Rejected");
+        }
+    }
 
     public static void main(String[] args) {
         Map<Character,NonTerminal> nonTerminalMap = new HashMap<>();
-        ArrayList<NonTerminal> nonTerminalList = new ArrayList<NonTerminal>();
         ArrayList<Character> terminal = new ArrayList<Character>();
+        String initial = "";
 
         Scanner in = new Scanner(System.in);
         System.out.println("Name of file(must be in the same folder): ");
@@ -65,8 +142,7 @@ public class PushDownAutomaton
             }
 
             else if(i == 3){
-                char a = s.charAt(0);
-                char initial = a;
+                initial = s;
             }
 
             else{
@@ -84,6 +160,10 @@ public class PushDownAutomaton
                 System.out.println("Derivation " + k + ": " + entry.getValue().getDerivations().get(k));
             }
         }
+
+        System.out.println("String: ");
+        String p = in.nextLine(); 
+        makeTree(nonTerminalMap,initial,p);
 
     }
 
